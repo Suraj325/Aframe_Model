adf50ef4ac883a221bef2d82a6ca22d0037f97eb
Suraj <68837927+Suraj325@users.noreply.github.com>
2021-12-07 15:37:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,34 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+$(window).load(function () {
+  $(".status").fadeOut();
+  $(".preloader").delay(50).fadeOut(1000);
+  $("body").delay(50).css({ overflow: "visible" });
+});
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-function menuToggle() {
-  const toggleMenu = document.querySelector(".toggleMenu");
-  toggleMenu.classList.toggle("active");
-}
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+const counters = document.querySelectorAll(".counter");
+const speed = 60;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(counter.getAttribute("data-target"));
+    const count = parseInt(counter.innerText);
+
+    const inc = Math.trunc(target / speed);
+
+    if (count < target) {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 45);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCount();
+});
